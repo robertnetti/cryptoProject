92915efcedcc5be59111d42b7f2e15b0db55f255
Anisha KC <anisha.kc7219@gmail.com>
2020-11-22 13:08:48 -0500

    some css and routing

--- main.py
+++ main.py
@@ -8,10 +8,26 @@ def home():
     return render_template("home.html")
 
 
+@app.route("/affine")
+def affine():
+    return render_template("affine.html")
+
+@app.route("/columnar")
+def columnar():
+    return render_template("columnar.html")
+
 @app.route("/about")
 def about():
     return render_template("about.html")
 
+@app.route("/rsa")
+def rsa():
+    return render_template("rsa.html")
+
+
+@app.route("/substitution")
+def substitution():
+    return render_template("substitution.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
