2ae77ad78d1a37e06f5d18f875f490cfb3c0b76e
Anisha KC <anisha.kc7219@gmail.com>
2020-11-22 19:44:16 -0500

    set up stuff to pass data to be encrypted

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template, redirect, url_for
 
 app = Flask(__name__)
 
@@ -7,10 +7,21 @@ app = Flask(__name__)
 def home():
     return render_template("home.html")
 
-
 @app.route("/affine")
 def affine():
     return render_template("affine.html")
+
+@app.route('/affineEncrypt', methods=['POST'])
+def affineEncrypt():
+    text = request.form["affine-encrypt"]
+    sending = text + " ENCRYPTED"
+    return render_template("affine.html", encrypted=sending)
+
+@app.route('/affineDecrypt', methods=['POST'])
+def affineDecrypt():
+    text = request.form["affine-decrypt"]
+    sending = text + " DECRYPTED"
+    return render_template("affine.html", decrypted=sending)
 
 @app.route("/columnar")
 def columnar():
