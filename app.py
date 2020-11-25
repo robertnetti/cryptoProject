c3676ba0f6e5210f66a4353fb851f84edbc9c56b
Anisha KC <anisha.kc7219@gmail.com>
2020-11-24 20:24:02 -0500

    code clean up

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, render_template, redirect, url_for
-from affine import *
-from rsa import *
-from substitution import *
+from affine import encrypt, decrypt
+from rsa import rsaencrypt, rsadecrypt, modinv
+from substitution import sub_encrypt, sub_decrypt
 
 app = Flask(__name__)
-
+affineHTML = "affine.html"
+rsaHTML = "rsa.html"
+subsHTML = "substitution.html"
+columnarHTML = "columnar.html"
 
 @app.route("/")
 def home():
@@ -12,7 +15,7 @@ def home():
 
 @app.route("/affine")
 def affine():
-    return render_template("affine.html")
+    return render_template(affineHTML)
 
 @app.route('/affineEncrypt', methods=['POST'])
 def affineEncrypt():
@@ -21,7 +24,7 @@ def affineEncrypt():
     a = int(request.form["addKeyAffine"])
     key = [m, a]
     sending = encrypt(text, key)
-    return render_template("affine.html", encrypted=sending)
+    return render_template(affineHTML, encrypted=sending)
 
 @app.route('/affineDecrypt', methods=['POST'])
 def affineDecrypt():
@@ -30,27 +33,27 @@ def affineDecrypt():
     a = int(request.form["addKeyAffineDec"])
     key = [m, a]
     sending = decrypt(text, key)
-    return render_template("affine.html", decrypted=sending)
+    return render_template(affineHTML, decrypted=sending)
 
 @app.route("/columnar")
 def columnar():
-    return render_template("columnar.html")
+    return render_template(columnarHTML)
 
 @app.route('/columnarEncrypt', methods=['POST'])
 def columnarEncrypt():
     text = request.form["columnar-encrypt"]
     sending = text + " COLUMNAR ENCRYPTED"
-    return render_template("columnar.html", encrypted=sending)
+    return render_template(columnarHTML, encrypted=sending)
 
 @app.route('/columnarDecrypt', methods=['POST'])
 def columnarDecrypt():
     text = request.form["columnar-decrypt"]
     sending = text + " COLUMNAR DECRYPTED"
-    return render_template("columnar.html", decrypted=sending)
+    return render_template(columnarHTML, decrypted=sending)
 
 @app.route("/rsa")
 def rsa():
-    return render_template("rsa.html")
+    return render_template(rsaHTML)
 
 @app.route('/rsaEncrypt', methods=['POST'])
 def rsaEncrypt():
@@ -58,7 +61,7 @@ def rsaEncrypt():
     e = int(request.form["eEncrypt"])
     N = int(request.form["nEncrypt"])
     sending = rsaencrypt(e, N, plaintext)
-    return render_template("rsa.html", encrypted=sending)
+    return render_template(rsaHTML, encrypted=sending)
 
 @app.route('/rsaDecrypt', methods=['POST'])
 def rsaDecrypt():
@@ -70,26 +73,26 @@ def rsaDecrypt():
     N = p*q
     d = modinv(e, phiN)
     sending = rsadecrypt(d, N, ciphertext)
-    return render_template("rsa.html", decrypted=sending)
+    return render_template(rsaHTML, decrypted=sending)
 
 
 @app.route("/substitution")
 def substitution():
-    return render_template("substitution.html")
+    return render_template(subsHTML)
 
 @app.route('/substitutionEncrypt', methods=['POST'])
 def substitutionEncrypt():
     plain_text = request.form["substitution-encrypt"]
     key = int(request.form["subKeyEnc"])
     sending = sub_encrypt(key, plain_text)
-    return render_template("substitution.html", encrypted=sending)
+    return render_template(subsHTML, encrypted=sending)
 
 @app.route('/substitutionDecrypt', methods=['POST'])
 def substitutionDecrypt():
     cipher_text = request.form["substitution-decrypt"]
     key = int(request.form["subKeyDec"])
     sending = sub_decrypt(key, cipher_text)
-    return render_template("substitution.html", decrypted=sending)
+    return render_template(subsHTML, decrypted=sending)
 
 @app.route("/about")
 def about():
